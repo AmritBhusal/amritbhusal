6320cc888b0c1262ca3ea77f3eb7290054a1ea11
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2025-02-12 10:15:11 +0545

    detail page minor update

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,4 +1,13 @@
+
 import Detail from "@/components/Portfolio/Detail";
+import portfolioData from '@/components/Portfolio/portfolio.json';
+
+// Generate static params for all project IDs
+export async function generateStaticParams() {
+  return portfolioData.map((project) => ({
+    id: project.id,
+  }));
+}
 
 const DetailPage = () => {
   return <Detail />;
